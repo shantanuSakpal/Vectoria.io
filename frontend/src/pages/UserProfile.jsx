81f3777f95301d92dbb18afe727c1a3e556d086c
Neil Shukla <neilpshukla221203@gmail.com>
2023-03-11 22:39:55 +0530

    photographer route added

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,19 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export default function UserProfile() {
     const useremail = useParams().email
+    const [data, setData] = useState()
     const GetRequest = async () => {
     const res = await fetch(`http://localhost:3001/user/user/${useremail}`)
     const json = await res.json();
     console.log(json)
+    setData(json)
     }
 
     useEffect(()=>{
         GetRequest()
     },[])
 
+    useEffect(()=>{
+        setData(data)
+        console.log(data)
+    },[data])
+    if(data)
     return (
-        <div>UserProfile</div>
+        <>
+        {data.map((i)=>{
+            return (
+                <>
+                <div key={i.id}>
+                {i.email}
+                {i.username}
+                {i.location}
+                </div>
+                </>
+                )
+        })}
+        </>
     )
 }
