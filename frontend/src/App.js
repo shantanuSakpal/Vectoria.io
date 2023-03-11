88e7a36452da276365c5dbcbfbcf7a9df16ad4d7
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-03-11 16:59:41 +0530

    landing page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
 import { Home } from "./pages/Home";
 import { Landing } from "./pages/Landing";
 // import Login from "./pages/login/Login";
@@ -10,22 +11,19 @@ import { Landing } from "./pages/Landing";
 const App = () => {
 
   return (
-    <div className="p-2 bg-blue-100 ">
 
-      <Router>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/home' element={<Home />} />
 
-        </Routes>
-        <div className="bottom-0 rounded-lg text-center w-full bg-blue">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
 
-          {/* <Footer /> */}
-        </div>
-      </Router>
+      </Routes>
 
-    </div>
+    </Router>
+
+
 
   )
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+
+    extend: {
+      fontFamily: {
+        'vonique': ['vonique-bold', 'Arial Narrow', "Arial", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
