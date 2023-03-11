ed80738402ccef77e1f57592fcba9f6594b4bceb
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-03-11 13:10:37 +0530

    home page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Landing } from "./pages/Landing";
+// import Login from "./pages/login/Login";
+// import SignUp from "./pages/login/SignUp";
 
-function App() {
+
+
+const App = () => {
+
   return (
-    <div className="App">
+    <div className="p-2 bg-blue-100 ">
+
+      <Router>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/home' element={<Home />} />
+
+        </Routes>
+        <div className="bottom-0 rounded-lg text-center w-full bg-blue">
+
+          {/* <Footer /> */}
+        </div>
+      </Router>
+
     </div>
-  );
-}
+
+  )
+};
 
 export default App;
